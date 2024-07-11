f1b73dc19be5fbaf451eb668194d61295f6a7957
Dan Muraya <duncan.muraya@cellulant.io>
2024-07-11 10:19:04 +0300

    addition of graphql

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("com.netflix.dgs.codegen") version "6.2.1"
 }
 
 group = "com.techpoa"
@@ -31,8 +32,10 @@ object Versions {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("io.temporal:temporal-sdk:${Versions.temporalVersion}")
     implementation("io.temporal:temporal-testing:${Versions.temporalVersion}")
@@ -42,6 +45,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.springframework.graphql:spring-graphql-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.testcontainers:junit-jupiter")
